9e792252beb5af23340d3d85e47fda2f8188b72a
echomicke <michael.kamara@chasacademy.se>
2023-04-12 15:47:24 +0200

    Added the GetAllEntries method and passed the second test

--- guestbook.py
+++ guestbook.py
@@ -3,3 +3,10 @@ FILE_NAME = "guestbook.txt"
 def AddEntry(entry):
     with open(FILE_NAME, "a+") as f:
         f.write(f"{entry}\n")
+
+def GetAllEntries():
+    entries = ""
+    with open(FILE_NAME, "r") as f:
+        entries = f.read()
+    print(entries)
+    return entries
