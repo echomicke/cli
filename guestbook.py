bb0f2b9ec23ad71a0d3c46e09a980dfac0255451
Michael Kamara <michael-kamara@outlook.com>
2023-05-28 18:36:04 +0200

    Made some changes to better run the application

--- guestbook.py
+++ guestbook.py
@@ -1,3 +1,4 @@
+"""A guestbook cli application"""
 import sys
 import json
 
@@ -5,19 +6,23 @@ FILE_NAME = "guestbook.txt"
 
 def GetFileContents():
     """Returns The contents of the guestbook textfile"""
-    with open(FILE_NAME, "r") as f:
-        entries = f.read()
-    return entries
+    try:
+        with open(FILE_NAME, "r", encoding="utf-8") as f:
+            entries = f.read()
+        return entries
+    except FileNotFoundError:
+        print("The guestbook has not been created or could not be found")
+        sys.exit()
 
 def ReplaceFileContent(new_entries):
     """Takes in a list and replaces the contents of the guestbook textfile with the list"""
-    with open(FILE_NAME, "w") as f:
+    with open(FILE_NAME, "w", encoding="utf-8") as f:
         f.write('\n'.join(new_entries))
         f.write("\n")
 
 def AddEntry(entry):
     """Adds an entry to the end of a file. Creates a new file if the file is missing"""
-    with open(FILE_NAME, "a+") as f:
+    with open(FILE_NAME, "a+", encoding="utf-8") as f:
         f.write(f"{entry}\n")
 
 def PrintEntries():
@@ -52,7 +57,18 @@ def Export():
     print (json_data)
     return json_data
 
+def deleteEmptyRowsInFile():
+    """Deletes all empty rows in the file"""
+    entries = GetFileContents()
+    try:
+        entries = entries.splitlines()
+        entries = [x for x in entries if x != '']
+        ReplaceFileContent(entries)
+    except AttributeError:
+        pass
+
 def Menu():
+    """Main menu for the program"""
     if sys.argv[1] == "new":
         AddEntry(sys.argv[2])
 
@@ -61,12 +77,16 @@ def Menu():
 
     elif sys.argv[1] == "edit":
         EditEntry(sys.argv[2], sys.argv[3])
-    
+
     elif sys.argv[1] == "delete":
         DeleteEntry(sys.argv[2])
 
     elif sys.argv[1] == "export":
         Export()
 
+#PrintEntries()
+#deleteEmptyRowsInFile()
+
 if __name__ == '__main__':
     Menu()
+    deleteEmptyRowsInFile()
